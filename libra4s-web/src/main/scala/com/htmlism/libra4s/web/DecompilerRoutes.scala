fec0232bf287a4e63e88ba1ac30d4027ff2c0c69
Mark Canlas <github@htmlism.com>
2026-05-01 17:36:42 -0400

    serve scalatags HTML form at root :globe_with_meridians:

--- libra4s-web/src/main/scala/com/htmlism/libra4s/web/DecompilerRoutes.scala
+++ libra4s-web/src/main/scala/com/htmlism/libra4s/web/DecompilerRoutes.scala
@@ -1,12 +1,22 @@
 package com.htmlism.libra4s.web
 
+import cats.data.*
 import cats.effect.IO
-import org.http4s.*
 import org.http4s.dsl.io.*
+import org.http4s.scalatags.*
+import org.http4s.{scalatags as _, *}
+
+import com.htmlism.libra4s.core.ProcessRunner
 
 object DecompilerRoutes:
   val routes: HttpRoutes[IO] =
     HttpRoutes.of[IO]:
       case GET -> Root =>
-        for res <- Ok("Decompiler endpoint is up")
+        Ok(DecompilerRoutesHtml.index)
+
+      case POST -> Root / "list" =>
+        for
+          xs <- ProcessRunner.run(NonEmptyList.of("ls", "-l", "/tmp"))
+
+          res <- Ok(xs.mkString("\n"))
         yield res
--- libra4s-web/src/main/scala/com/htmlism/libra4s/web/DecompilerRoutesHtml.scala
+++ libra4s-web/src/main/scala/com/htmlism/libra4s/web/DecompilerRoutesHtml.scala
@@ -1,3 +1,19 @@
 package com.htmlism.libra4s.web
 
-object DecompilerRoutesHtml
+import scalatags.Text
+import scalatags.Text.all.*
+
+object DecompilerRoutesHtml:
+  val index: Text.TypedTag[String] =
+    html(
+      head(
+        meta(charset := "UTF-8"),
+        tag("title")("libra4s")
+      ),
+      body(
+        h1("libra4s"),
+        form(method := "POST", action := "/list")(
+          button(`type` := "submit")("List /tmp")
+        )
+      )
+    )
